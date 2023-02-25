5f1dd99ed4a719b4f64441ba8087295b8b834bbd
David Nunez <davidnunez@Davids-MacBook-Air.local>
2023-02-25 00:48:09 -0800

    api module authenticates itself on initialization rather than on each request

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const api = require('/Users/davidnunez/Documents/protoWebsite/server_scripts/api')
+const getTrack = api.getTrack
+const getAlbum = api.getAlbum
+
+router.use(express.static('public'))
 
 router.get('/', (req, res) => {
     console.log('request "/" received')
-    res.send('Homepage')
+    res.sendFile('public/index.html')
 })
 
-router.get('/token', (req, res) => {
+router.get('/track', (req, res) => {
     console.log('request "/token" received')
-    api.getAuth()
-    .then(result => {
-        res.send(result)
-    })
+    getTrack()
+        .then(result => {
+            res.send(result)
+        })
 })
 
-router.get('/data', (req, res) => {
+router.get('/album', (req, res) => {
     console.log('request "/data" received')
-    res.send('Showing data...')
+    getAlbum()
+        .then(result => {
+            res.send(result)
+        })
 })
 
 module.exports = router
--- server_scripts/api.js
+++ server_scripts/api.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 const clientID = 'ae347d7db2a34bc9abd757c6e41a5ca9';
 const clientSecret = 'da818a429104499ea25921dac5fcd6b6';
+const spotifyUrl = 'https://api.spotify.com/v1'
+let clientToken = ''
 
 const auth = async () => {
     const res = await axios.post('https://accounts.spotify.com/api/token', { grant_type: 'client_credentials' }, {
@@ -8,18 +10,39 @@ const auth = async () => {
         'Authorization': 'Basic YWUzNDdkN2RiMmEzNGJjOWFiZDc1N2M2ZTQxYTVjYTk6ZGE4MThhNDI5MTA0NDk5ZWEyNTkyMWRhYzVmY2Q2YjY=',
         'Content-Type': 'application/x-www-form-urlencoded'
     }})
-    return res.data.access_token
+    clientToken = res.data.access_token
 }
 
 const getTrack = async () => {
-    const token = await auth()
-    const res = await axios.get('https://api.spotify.com/v1/tracks/33bsk1Zn8QAAJnE7erlCtP', {
+    const res = await axios.get(`${spotifyUrl}/tracks/33bsk1Zn8QAAJnE7erlCtP`, {
         headers: {
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${clientToken}`
         }
     })
-    //sends back info for given track in JSON
+    //Logic for expired token
+    if(res.status == 401){
+        console.log(res.status+ '\nBad or expired token, refreshing token and retrying automatically...')
+        auth()
+        getTrack()
+    }
+    //Send response back
     return res.data
 }
 
-module.exports = auth, getTrack
+const getAlbum = async () => {
+    const res = await axios.get(`${spotifyUrl}/albums/13PxecK9Bart7ir6STafXP`, {
+        headers: {
+            'Authorization': `Bearer ${clientToken}`
+        }
+    })
+    console.log(res.status)
+    return res.data
+}
+
+auth()
+console.log('api has been loaded')
+
+module.exports = {
+    getTrack,
+    getAlbum
+}
